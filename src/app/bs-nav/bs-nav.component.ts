3b8570c372a67de41e1ca7f9a219c23bebad1760
Shen Qu <qushen024@hotmail.com>
2020-12-16 16:01:05 -0500

    Implementing Logout

--- src/app/bs-nav/bs-nav.component.ts
+++ src/app/bs-nav/bs-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-bs-nav',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BsNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(s => console.log(s));
+  }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.afAuth.signOut();
+  }
 }
