b4239a3650e97aec6fbec883b3694fc8290550dc
Shen Qu <qushen024@hotmail.com>
2020-12-16 13:15:08 -0500

    Cleaning Up the Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { CheckOutComponent } from './check-out/check-out.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
@@ -16,6 +17,7 @@ const routes: Routes = [
   {path: 'shopping-cart', component: ShoppingCartComponent},
   {path: 'check-out', component: CheckOutComponent},
   {path: 'order-success', component: OrderSuccessComponent},
+  {path:'my/orders', component: MyOrdersComponent},
   {path: 'login', component: LoginComponent},
   {path: 'admin/products', component: AdminProductsComponent},
   {path: 'admin/orders', component: AdminOrdersComponent},
