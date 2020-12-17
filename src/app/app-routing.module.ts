dc7608e12a9e914cf36c953de51e8ac68c6a86b3
Shen Qu <qushen024@hotmail.com>
2020-12-17 00:14:58 -0500

    Implementing redirecting after logging in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ const routes: Routes = [
   {path: 'check-out', component: CheckOutComponent, canActivate:[AuthGuard]},
   {path: 'order-success', component: OrderSuccessComponent, canActivate:[AuthGuard]},
   {path:'my/orders', component: MyOrdersComponent, canActivate:[AuthGuard]},
+
   {path: 'admin/products', component: AdminProductsComponent, canActivate:[AuthGuard]},
   {path: 'admin/orders', component: AdminOrdersComponent, canActivate:[AuthGuard]},
 ];
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { ActivatedRoute, Router } from '@angular/router';
 import firebase from 'firebase/app';
 import { Observable } from 'rxjs';
 
@@ -10,11 +11,13 @@ export class AuthService {
 
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private router: Router) {
     this.user$ = afAuth.authState;
    }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
     this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
