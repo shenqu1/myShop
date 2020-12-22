c33ec7d16f93ccb857c367d8805ce5bdbe2b1d8c
Shen Qu <qushen024@hotmail.com>
2020-12-21 22:30:36 -0500

    Add to cart feature

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'app-product-card',
@@ -10,9 +12,13 @@ export class ProductCardComponent implements OnInit {
   @Input('product') product;
   @Input('showAction') showAction = true;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit(): void {
+  }
+
+  addToCart(product: Product) {
+    this.shoppingCartService.addToCart(product);
   }
 
 }
---
+++ src/app/shopping-cart.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Product } from './models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  private creat() {
+    return this.db.list('/shopping-carts').push({
+      dateCreated: new Date().getTime()
+    });
+  }
+
+  private getCart(cardId: string) {
+    return this.db.object('/shopping-carts/' + cardId).valueChanges()
+  }
+
+  private getItem(cardId: string, productId: string): AngularFireObject<any> {
+    return this.db.object('/shopping-carts/' + cardId + '/items/' + productId);
+  }
+
+  private async getOrCreatCartId() {
+    let cartId = localStorage.getItem('cartId');
+
+    if(!cartId) {
+      let result = await this.creat();
+      localStorage.setItem('cartId', result.key);
+      return result.key;
+    } else
+    return cartId;
+  }
+
+  async addToCart(product: Product) {
+    let cardId = await this.getOrCreatCartId();
+    let item$ = this.getItem(cardId, product.key);
+    item$.valueChanges().pipe(take(1)).subscribe(item => item$.update({product: product, quantity: (item ? item.quantity : 0) + 1}));
+  }
+}
