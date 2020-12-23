167c94d9c4a70858017aee423f2339972cfb482e
Shen Qu <qushen024@hotmail.com>
2020-12-23 11:40:54 -0500

    Shipping form

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-check-out',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckOutComponent implements OnInit {
 
+  form = new FormGroup({
+    'name': new FormControl('', [Validators.required]),
+    'addressLine1': new FormControl('', [Validators.required]),
+    'addressLine2': new FormControl(''),
+    'city': new FormControl('', [Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+
+  get addressLine1() {
+    return this.form.get('addressLine1');
+  }
+
+  get addressLine2() {
+    return this.form.get('addressLine2');
+  }
+
+  get city() {
+    return this.form.get('city');
+  }
+
+  onSubmit() {
+    console.log(this.form);
   }
 
 }
